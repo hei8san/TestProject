ea587cd39c33ae51aed2f5611e33ce9097121878
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-14 14:01:20 -0700

    test3

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
-		System.out.println("Nice to meet you!!");
+		System.out.println("Nice to meet you!!"); 
+		System.out.println();
 
 	}
 
